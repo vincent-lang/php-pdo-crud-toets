e0a9cdd0f07670a7b95e35f47655bca173607e00
vincent van de merwe <335479@student.mboutrecht.nl>
2023-01-12 14:33:46 +0100

    zeventh commit

--- read.php
+++ read.php
@@ -28,3 +28,37 @@ $statement = $pdo->prepare($sql);
 $statement->execute();
 
 $result = $statement->fetchAll(pdo::FETCH_OBJ);
+
+$rows = "";
+foreach ($result as $info) {
+    $rows .= "<tr>
+                <td>$info->Id</td>
+                <td>$info->Merk</td>
+                <td>$info->Model</td>
+                <td>$info->Topsnelheid</td>
+                <td>$info->Prijs</td>
+                <td>
+                    <a href='delete.php?id={$info->Id}'>
+                        <img src='img/b_drop.png' alt='drop'</img>
+                    </a>
+                </td>
+            </tr>";
+}
+
+?>
+
+<a href="index.html">Home Page</a>
+<h3>De vijf duurste auto's ter wereld</h3>
+<table border="1">
+    <thead>
+        <th>Id</th>
+        <th>Merk</th>
+        <th>Model</th>
+        <th>Topsnelheid</th>
+        <th>Prijs</th>
+        <th>Delete</th>
+    </thead>
+    <tbody>
+        <?= $rows; ?>
+    </tbody>
+</table>
