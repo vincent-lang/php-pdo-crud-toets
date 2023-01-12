f08132880a2659f0ff4c7d4d4988b8ddd32d58fa
vincent van de merwe <335479@student.mboutrecht.nl>
2023-01-12 14:10:37 +0100

    fourth commit

--- create.php
+++ create.php
@@ -1,3 +1,35 @@
 <?php
 
 require("config.php");
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+    } else {
+    }
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+$sql = "INSERT INTO DureAuto (Id
+                            ,Merk
+                            ,Model
+                            ,Topsnelheid
+                            ,Prijs)
+                    VALUES  (NULL
+                            ,:Merk
+                            ,:Model
+                            ,:Topsnelheid
+                            ,:Prijs);";
+$statement = $pdo->prepare($sql);
+
+$statement->bindValue(":Merk", $_POST['Merk'], PDO::PARAM_STR);
+$statement->bindValue(":Model", $_POST['Model'], PDO::PARAM_STR);
+$statement->bindValue(":Topsnelheid", $_POST['Topsnelheid'], PDO::PARAM_STR);
+$statement->bindValue(":Prijs", $_POST['Prijs'], PDO::PARAM_STR);
+
+$statement->execute();
+
+header("location: read.php");
